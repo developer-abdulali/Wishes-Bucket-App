9dfec7ea8266cb009ff11b03040d8ace07db3fb5
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-23 01:03:10 +0500

    added changes

--- src/components/AddWish.js
+++ src/components/AddWish.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
+import { WishContext } from "../context/WishProvider";
 
 export default function AddWish() {
+  const [wishes, setWishes] = useContext(WishContext);
+  const [title, setTitle] = useState("");
+
+  const updateTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const addWish = (e) => {
+    e.preventDefault();
+    setWishes((prevWishes) => [
+      ...prevWishes,
+      { title: title, id: prevWishes[prevWishes.length - 1].id + 1 },
+    ]);
+    setTitle("");
+  };
+
   return (
     <div>
-        <form className='form'>
-            <input type='text' className='addWish' placeholder='Add a new wish' name='title' value="New Task" />
-            <input type='submit' className='sbmt' value='Add Task' />
-            
-        </form>
+      <form className="form">
+        <input
+          type="text"
+          className="addWish"
+          placeholder="Add a new wish..."
+          name="title"
+          value={title}
+          onChange={updateTitle}
+        />
+        <input
+          onClick={addWish}
+          type="submit"
+          className="sbmt"
+          value="Add Task"
+        />
+      </form>
     </div>
-  )
+  );
 }
